201144dcc19cc4c31bce2024bb68f4b7145891ff
saikrishna6415 <saikrishnakotagiri16@gmail.com>
2019-12-11 12:14:41 +0530

    Added new file

---
+++ inversetrainglenumbers.py
@@ -0,0 +1,9 @@
+n = int(input("enter a number: "))
+a=1
+for i in range(1,n+3):
+    print(i*" ",end=" ")
+    for j in range(n-i+1):
+        print((str(a)+" "),end="")
+        a+=(n-j)
+    print()
+    a=i+1
